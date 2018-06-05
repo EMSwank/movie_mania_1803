73d10c2948ceb0276c0d63410401f88a7f145d51
Eliot Swank <eliotswank@gmail.com>
2018-06-05 09:12:39 -0600

    creates first user story test

---
+++ spec/features/adimn_visits_genre_page_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "An admin" do
+  context "visits the genre index page" do
+    it "and sees a list of all genre names in the database." do
+      genre1 = Genre.create(name: 'genre1')
+      genre2 = Genre.create(name: 'genre2')
+
+      visit admin_genres_page
+
+      expect(page).to have_content(genre1.name)
+      expect(page).to have_content(genre2.name)
+    end
+  end
+end
