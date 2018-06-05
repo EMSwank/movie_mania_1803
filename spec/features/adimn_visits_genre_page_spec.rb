6edbfb6e9c97314f5dd9a2d193e188ebdd5ce793
Eliot Swank <eliotswank@gmail.com>
2018-06-05 09:55:45 -0600

    adds form to admin genre index

--- spec/features/adimn_visits_genre_page_spec.rb
+++ spec/features/adimn_visits_genre_page_spec.rb
@@ -14,5 +14,18 @@ describe "An admin" do
       expect(page).to have_content(genre1.name)
       expect(page).to have_content(genre2.name)
     end
+
+    it "and sees a form to create a new genre." do
+      user = User.create(username: 'user', password: 'password', role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      genre1 = Genre.create(name: 'genre1')
+      genre2 = Genre.create(name: 'genre2')
+
+      visit admin_genres_path
+      fill_in 'genre[name]', with: 'name'
+      expect(page).to have_content("Name:")
+    end
+    
   end
 end
