372551cbdaa9949d00d0ed05372b2d578ea2b5f9
Eliot Swank <eliotswank@gmail.com>
2018-06-05 09:19:15 -0600

    adds test for model relationships

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:genre_movies) }
+  it { should have_many(:movies).through(:genre_movies) }  
 end
