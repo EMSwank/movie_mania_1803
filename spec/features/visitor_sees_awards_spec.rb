28af8b0fbd72419eda8fa4efa1fe0345fa431cf0
Eliot Swank <eliotswank@gmail.com>
2018-06-06 09:48:25 -0600

    finishes story 3

--- spec/features/visitor_sees_awards_spec.rb
+++ spec/features/visitor_sees_awards_spec.rb
@@ -5,14 +5,29 @@ describe "A visitor" do
     it "does not see a for to create new awards" do
       visit awards_path
 
-      expect(page).to_not have_filed('award[name]') 
+      expect(page).to_not have_content('award[name]') 
+    end
+
+    it "sees all the awards" do
+      award1 = Award.create(name: 'award1')
+      award2 = Award.create(name: 'award2')
+
+      visit awards_path
+
+      expect(page).to have_content(award1.name) 
+      expect(page).to have_content(award2.name) 
+    end
+
+    it "have link to award show page" do
+      award1 = Award.create(name: 'award1')
+      award2 = Award.create(name: 'award2')
+
+      visit awards_path
+      click_on award1.name
+
+      expect(current_path).to eq(award_path(award1))
     end
   end
 end
 
 
-# As a Visitor,
-#   When I visit the awards index,
-#     I cannot see the form to create new awards, 
-#     I see all awards
-#       and each award should be a link to that award's specific page.
--- spec/models/award_movie_spec.rb
+++ spec/models/award_movie_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe AwardMovie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+it { should belong_to(:award) } 
+it { should belong_to(:movie) } 
 end
